e542fd3f1a26654f601e4f161c07c84d2ca49913
LiciuStefan <89767945+LiciuStefan@users.noreply.github.com>
2023-01-10 14:16:29 +0100

    Delete SAE Prog directory

--- SAE Prog/formulaire/deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); 
-session_destroy(); // On va detruire la session et ramener l'utilisateur au menu principal
-header('Location: ../index.php');
-exit();
-?>
