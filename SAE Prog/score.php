3d719a671be718505dee6431cbc66df57e8febc8
rayanesabi <92162339+rayanesabi@users.noreply.github.com>
2023-01-11 12:16:41 +0100

    stocke le temps dans la base de données

--- SAE Prog/score.php
+++ SAE Prog/score.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
-$conn = new PDO('sqlite:db/db_membre.sqlite3');
-var_dump($_POST['temps']);
-$temps = $_POST['temps'];
+$conn = new PDO('sqlite:db/bd_marche.sqlite3');
 
-echo $temps;
+// Récupération de la variable "data" envoyée par la fonction AJAX
+$temps = $_POST['data'];
+if ($temps != null) {
+    // Insertion de la variable dans la base de données
+    $query = $conn->prepare("INSERT INTO score (temps) VALUES (:temps)");
+    $query->bindParam(':temps', $temps);
+    $query->execute();
+    header('Location: page5.php');
+}
+?>
