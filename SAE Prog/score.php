4990dda141f3a6b96847605b7a9c3b07121d0ad4
Sofian Zerkouni <91824277+sofian13@users.noreply.github.com>
2023-01-12 15:15:14 +0100

    tableau des scores

--- SAE Prog/score.php
+++ SAE Prog/score.php
@@ -4,11 +4,16 @@ $conn = new PDO('sqlite:db/bd_marche.sqlite3');
 
 // Récupération de la variable "data" envoyée par la fonction AJAX
 $temps = $_POST['data'];
+$email = $_POST['mail'];
+$mem_id = $conn->prepare('select mem_id from membre where mail = :mail');
+$mem_id->execute();
+$test = $mem_id -> fetch();
 if ($temps != null) {
     // Insertion de la variable dans la base de données
-    $query = $conn->prepare("INSERT INTO score (temps) VALUES (:temps)");
+    $query = $conn->prepare("INSERT INTO score (temps, mem_id) VALUES (:temps, :mem_id)");
     $query->bindParam(':temps', $temps);
+    $query->bindParam(':mem_id', $email);
     $query->execute();
-    header('Location: page5.php');
+    header('Location: tableau_score.php');
 }
 ?>
---
+++ SAE Prog/tableau_score.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+$pdo = new PDO('sqlite:db/bd_marche.sqlite3');
+// Requête pour récupérer les données de la table
+
+$membres = $pdo->prepare('select pseudo, temps from membre, score where membre.mem_id=score.mem_id ORDER BY temps ASC');
+$membres->execute();
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title> Tableau de score </title>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 80%;
+            margin: auto;
+        }
+
+        th, td {
+            border: 1px solid #3c6382;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #3c6382;
+            color: white;
+            font-weight: bold;
+        }
+
+        tr:nth-child(even) {
+            background-color: #d9edf7;
+        }
+        tr:nth-child(odd) {
+            background-color: #ffffff;
+        }
+
+    </style>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Pseudo</th>
+            <th>Temps</th>
+        </tr>
+        <?php while ($test = $membres -> fetch() )
+  { ?>
+        <tr>
+            <td><?= $test['pseudo'] ?></td>
+            <td><?= $test['temps'] ?></td>
+        </tr>
+        <?php } ?>
+    </table>
+</body>
+
+</html>
+
