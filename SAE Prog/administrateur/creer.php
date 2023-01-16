4db9e11e972c7ef99f76bbde8fe540a21832a796
Léo Veysseyre <92161817+LeoVeysseyre84@users.noreply.github.com>
2023-01-16 15:58:42 +0100

    Update creer.php

--- SAE Prog/administrateur/creer.php
+++ SAE Prog/administrateur/creer.php
@@ -12,55 +12,55 @@ session_start();
 </head>
 
 <body>
+
+    <a href="../choix_exercice.php"><img src="../images/logo.png" alt="Logo" class="logo"></a>
+
     <form method="POST" action="modification_exercice.php">
 
-                <div class="all-container">
+        <div class="all-container">
 
-                    <div class="left-flex-box">
+            <div class="left-flex-box">
 
-                        <div class="flex-container1">
-                            <label for="nom">Titre de l'exercice:</label><br>
-                            <textarea id="nom" name="nom" required="required"></textarea><br>
-                        </div>
+                <div class="flex-container1">
+                    <label for="nom">Titre de l'exercice:</label><br>
+                    <textarea id="nom" name="nom" required="required"></textarea><br>
+                </div>
 
-                        <div class="flex-container2">
-                            <label for="description">Description de l'exercice:</label><br>
-                            <textarea id="description" name="description"
-                                required="required"></textarea><br>
-
-                        </div>
-
-                        <div class="flex-container3">
-                            <label for="objectifs">Objectifs de l'exercice:</label><br>
-                            <textarea id="objectifs" name="objectifs"
-                                required="required"></textarea><br>
-                        </div>
-
-                        <div class="button">
-                            <button name="creer"> Confirmer la création</button>
-                        </div>
-
-                    </div>
-
-                    <div class="right-flex-box">
-
-                        <div class="flex-container4">
-                            <label for="contenu">Contenu de l'exercice:</label></br>
-                            <textarea id="contenu" name="contenu" required="required"></textarea><br>
-                        </div>
-
-                        <div class="flex-container5">
-                            <label for="testsUnitaires">Tests unitaires:</label></br>
-                            <textarea id="testsUnitaires" name="testsUnitaires"
-                                required="required"></textarea><br>
-                        </div>
-
-                    </div>
-
-
-
+                <div class="flex-container2">
+                    <label for="description">Description de l'exercice:</label><br>
+                    <textarea id="description" name="description" required="required"></textarea><br>
 
                 </div>
+
+                <div class="flex-container3">
+                    <label for="objectifs">Objectifs de l'exercice:</label><br>
+                    <textarea id="objectifs" name="objectifs" required="required"></textarea><br>
+                </div>
+
+                <div class="button">
+                    <button name="creer"> Confirmer la création</button>
+                </div>
+
+            </div>
+
+            <div class="right-flex-box">
+
+                <div class="flex-container4">
+                    <label for="contenu">Contenu de l'exercice:</label></br>
+                    <textarea id="contenu" name="contenu" required="required"></textarea><br>
+                </div>
+
+                <div class="flex-container5">
+                    <label for="testsUnitaires">Tests unitaires:</label></br>
+                    <textarea id="testsUnitaires" name="testsUnitaires" required="required"></textarea><br>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
     </form>
 
 </body>
