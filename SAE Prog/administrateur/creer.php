47c2b8daebf4dbc8dddd6bca11862802e185d28a
Léo Veysseyre <92161817+LeoVeysseyre84@users.noreply.github.com>
2023-01-16 13:59:21 +0100

    Add files via upload

--- SAE Prog/administrateur/creer.php
+++ SAE Prog/administrateur/creer.php
@@ -5,40 +5,62 @@ session_start();
 ?>
 
 <head>
-    <title>Creer exercice </title>
+    <title>Créer exercice </title>
     <link rel="icon" href="https://1000logos.net/wp-content/uploads/2020/09/Java-Emblem.jpg">
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1" />
-    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="../css/creation_modif.css" />
 </head>
-<div class="col-md-3"></div>
-<div class="col-md-6 well">
-    <h3 class="text-primary">Création exercice</h3>
-    <hr style="border-top:1px dotted #ccc;" />
-    <br style="clear:both;" /><br />
-    <div class="col-md-3"></div>
-    <div class="col-md-6">
-        <form method="POST" action="modification_exercice.php">
 
-            <div class="form-group">
+<body>
+    <form method="POST" action="modification_exercice.php">
 
-                <input type="mail" name="nom" class="form-control" required="required" />
-                <label>Nom de l'exercice</label>
-            </div>
-            <div class="form-group">
+                <div class="all-container">
 
-                <input type="text" name="description" class="form-control" required="required" />
-                <label>Description de l'exercice</label>
-            </div>
+                    <div class="left-flex-box">
 
-            <div class="form-group">
-                <input type="text" name="contenu" class="form-control" required="required" />
-                <label>Contenu de l'exercice</label>
-            </div>
-            <div class="button">
-                <div class="inner"></div>
-                <button name="creer"> Confirmer la création</button>
-            </div>
-        </form>
-    </div>
-</div>
+                        <div class="flex-container1">
+                            <label for="nom">Titre de l'exercice:</label><br>
+                            <textarea id="nom" name="nom" required="required"></textarea><br>
+                        </div>
+
+                        <div class="flex-container2">
+                            <label for="description">Description de l'exercice:</label><br>
+                            <textarea id="description" name="description"
+                                required="required"></textarea><br>
+
+                        </div>
+
+                        <div class="flex-container3">
+                            <label for="objectifs">Objectifs de l'exercice:</label><br>
+                            <textarea id="objectifs" name="objectifs"
+                                required="required"></textarea><br>
+                        </div>
+
+                        <div class="button">
+                            <button name="creer"> Confirmer la création</button>
+                        </div>
+
+                    </div>
+
+                    <div class="right-flex-box">
+
+                        <div class="flex-container4">
+                            <label for="contenu">Contenu de l'exercice:</label></br>
+                            <textarea id="contenu" name="contenu" required="required"></textarea><br>
+                        </div>
+
+                        <div class="flex-container5">
+                            <label for="testsUnitaires">Tests unitaires:</label></br>
+                            <textarea id="testsUnitaires" name="testsUnitaires"
+                                required="required"></textarea><br>
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
+    </form>
+
+</body>
