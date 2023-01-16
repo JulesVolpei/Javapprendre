e1b5e3bff101092973300111d9f0d738bd37b273
Léo Veysseyre <92161817+LeoVeysseyre84@users.noreply.github.com>
2023-01-16 14:22:18 +0100

    Update exercice.php

--- SAE Prog/exercice.php
+++ SAE Prog/exercice.php
@@ -74,7 +74,7 @@ catch(PDOException $e)
         <div class="nav-container">
           
         <div class="brand">
-          <a href="../index.php"><img src="../images/logo.png" alt="Logo"></a>
+          <a href="../choix_exercice.php"><img src="../images/logo.png" alt="Logo"></a>
         </div>
           
            <div name="affi" id="time"></div>  
